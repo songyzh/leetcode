d9e31db114be0ec06990e762ae997fedb0115b27
songyzh <mailsyzh@gmail.com>
2019-10-25 15:57:57 +0800

    ad

--- _118_pascals_triangle/Solution.java
+++ _118_pascals_triangle/Solution.java
@@ -2,7 +2,7 @@ package _118_pascals_triangle;
 /*
  * @Author: your name
  * @Date: 2019-10-24 09:12:58
- * @LastEditTime: 2019-10-24 09:18:43
+ * @LastEditTime: 2019-10-25 15:55:43
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /leetcode/_118_pascals_triangle/Solution.java
@@ -12,6 +12,7 @@ package _118_pascals_triangle;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 // 思路:
 // 可用dp的方法, 把之前的结果保存在集合中, 根据之前的层级, 得出新的层级
@@ -24,8 +25,17 @@ class Solution {
                 ret.add(Arrays.asList(1));
             } else if (i == 1) {
                 ret.add(Arrays.asList(1, 1));
-
+            } else {
+                List<Integer> prev = ret.get(ret.size() - 1);
+                List<Integer> current = new ArrayList<>();
+                ret.add(current);
+                current.add(1);
+                for (int j = 0; j < prev.size() - 1; ++j) {
+                    current.add(prev.get(j) + prev.get(j + 1));
+                }
+                current.add(1);
             }
         }
+        return ret;
     }
 }
